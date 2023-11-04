ed1c2398d19ef358f5519050b02cf6f18f5db3e2
Jai A <jaiagr+gpg@pm.me>
2023-11-03 19:43:35 -0700

    remove junk

--- src/index.ts
+++ src/index.ts
@@ -17,63 +17,63 @@ function makeError(code: number, error: String, description: String): Response {
 }
 
 interface UrlData {
-  projectId: string,
-  versionId: string,
+	projectId: string;
+	versionId: string;
 }
 
 function extractUrlData(path: string): UrlData | null {
-  // Pattern: data/:hash/versions/:version/:file
-  const parts = path.split('/');
-  console.log(parts)
+	// Pattern: data/:hash/versions/:version/:file
+	const parts = path.split('/');
+	console.log(parts);
 
-  const hashIndex = parts.findIndex(part => part === 'data') + 1;
-  const versionIndex = parts.findIndex(part => part === 'versions') + 1;
+	const hashIndex = parts.findIndex((part) => part === 'data') + 1;
+	const versionIndex = parts.findIndex((part) => part === 'versions') + 1;
 
-  console.log(hashIndex)
-  console.log(versionIndex)
+	console.log(hashIndex);
+	console.log(versionIndex);
 
-  if (hashIndex === -1 || versionIndex === -1) {
-    return null
-  }
+	if (hashIndex === -1 || versionIndex === -1) {
+		return null;
+	}
 
-  const projectId = parts[hashIndex];
-  const versionId = parts[versionIndex];
+	const projectId = parts[hashIndex];
+	const versionId = parts[versionIndex];
 
-  return { projectId, versionId };
+	return { projectId, versionId };
 }
 
 async function countDownload(request: Request, env: Env, urlData: UrlData) {
-  console.log(`Attempting to count download of project ID ${urlData.projectId} with version ID ${urlData.versionId}`)
+	console.log(`Attempting to count download of project ID ${urlData.projectId} with version ID ${urlData.versionId}`);
 
-  const url = `${env.LABRINTH_URL}admin/_count-download`
+	const url = `${env.LABRINTH_URL}admin/_count-download`;
 
-  console.log(`url: ${url}`)
+	console.log(`url: ${url}`);
 
-  let headersObj: Record<string, string> = {};
-  request.headers.forEach((value, key) => {
-    headersObj[key] = value;
-  });
+	let headersObj: Record<string, string> = {};
+	request.headers.forEach((value, key) => {
+		headersObj[key] = value;
+	});
 
-  const res = await fetch(
-    new Request(url, {
-      method: 'PATCH',
-      headers: {
-        'Modrinth-Admin': env.LABRINTH_ADMIN_KEY,
-        'x-ratelimit-key': env.RATE_LIMIT_IGNORE_KEY,
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        url: request.url,
-        project_id: urlData.projectId,
-        version_name: urlData.versionId,
-        ip: request.headers.get('CF-Connecting-IP') ?? '127.0.0.1',
-        headers: headersObj,
-      }),
-    })
-  )
+	const res = await fetch(
+		new Request(url, {
+			method: 'PATCH',
+			headers: {
+				'Modrinth-Admin': env.LABRINTH_ADMIN_KEY,
+				'x-ratelimit-key': env.RATE_LIMIT_IGNORE_KEY,
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({
+				url: request.url,
+				project_id: urlData.projectId,
+				version_name: urlData.versionId,
+				ip: request.headers.get('CF-Connecting-IP') ?? '127.0.0.1',
+				headers: headersObj,
+			}),
+		}),
+	);
 
-  console.log(`Finished counting download. Status code: ${res.status}`)
-  console.log(`Response body: ${await res.text()}`)
+	console.log(`Finished counting download. Status code: ${res.status}`);
+	console.log(`Response body: ${await res.text()}`);
 }
 
 export default {
@@ -89,22 +89,22 @@ export default {
 		const cache = caches.default;
 		const cacheResponse = await cache.match(request);
 
-    const url = new URL(request.url);
-    const key = url.pathname.slice(1);
+		const url = new URL(request.url);
+		const key = url.pathname.slice(1);
 
-    const urlData = extractUrlData(key)
+		const urlData = extractUrlData(key);
 
-    console.log(urlData)
-    if (urlData && request.method === 'GET') {
-      ctx.waitUntil(countDownload(request, env, urlData))
-    }
+		console.log(urlData);
+		if (urlData && request.method === 'GET') {
+			ctx.waitUntil(countDownload(request, env, urlData));
+		}
 
 		if (!cacheResponse || !(cacheResponse.ok || cacheResponse.status == 304)) {
-      console.warn('Cache miss. Fetching origin.')
+			console.warn('Cache miss. Fetching origin.');
 
-      if (key.endsWith('/')) {
-        return makeError(404, 'not_found', 'the requested resource does not exist');
-      }
+			if (key.endsWith('/')) {
+				return makeError(404, 'not_found', 'the requested resource does not exist');
+			}
 
 			const object: R2Object | R2ObjectBody | null =
 				request.method === 'HEAD' ? await env.MODRINTH_CDN.head(key) : await env.MODRINTH_CDN.get(key);
